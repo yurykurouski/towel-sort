6ab415fbfcca16b8e0812ce438d78ab6b440a010
Yury <ymorpheus@gmail.com>
2021-03-06 21:45:02 +0300

    feat: finish Towel Sort task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix) return [];
+
+  const result = [];
+
+  let index = 0;
+
+  matrix.forEach(el => {
+    if (index === 0) {
+      result.push(el);
+      index++
+    } else {
+      result.push(el.reverse());
+      index--;
+    }
+  });
+
+  return result.flat();
 }
